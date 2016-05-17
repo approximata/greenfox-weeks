84a02bd320e3b313d73caf8d3f5e265f40c4d62e
bodor mate <bodormate@gmail.com>
2016-05-17 15:23:20 +0200

    play with arrays

---
+++ week-03/day-1/17.py
@@ -0,0 +1,4 @@
+#!/usr/local/bin/python3
+q = [4, 5, 6, 7]
+# get the 3rd element of q
+print(q[1])
---
+++ week-03/day-1/18.py
@@ -0,0 +1,4 @@
+#!/usr/local/bin/python3
+r = [54, 23, 66, 12]
+# add the second the third
+print(r[1:3])
