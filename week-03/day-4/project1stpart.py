79444a60be4ed2b8c02e8d4fecf67d708d9d0618
bodor mate <bodormate@gmail.com>
2016-05-23 20:07:38 +0200

    remove unnecessary lines and method

--- week-03/day-4/project1stpart.py
+++ week-03/day-4/project1stpart.py
@@ -1,21 +1,8 @@
 names = ['laci', 'gabi', 'pisti', 'misi']
 
-def revers_words(words):
-    new_renum=[]
-    for i in range(len(words)-1, -1, -1):
-        new_renum.append(words[i])
-    return new_renum
-
-
-print(revers_words(names))
-
-
-name = "lacika"
-
 def revers_word(word):
     new_word = ''
     for i in range(len(word)-1, -1, -1):
         new_word += word[i]
-    return word + new_word 
-
+    return word + new_word
 print(revers_word(name))
