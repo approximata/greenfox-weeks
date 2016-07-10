a459b4f109954280391d2247ad1b1bf30dde59c9
bodor mate <bodormate@gmail.com>
2016-07-10 15:09:23 +0200

    add function working correctly

--- week-08/day-04/projecttodo/script.js
+++ week-08/day-04/projecttodo/script.js
@@ -1,38 +1,51 @@
 'use strict';
 var button = document.querySelector('.button-text');
-
 var todolist = document.querySelector('.todo-list');
+var inputField = document.querySelector('input');
 
-function addHtml(inputdata, index) {
+function addHtml(element) {
   var newTodo = document.createElement('div');
   newTodo.classList.add('taskholder');
-  newTodo.innerHTML = '<div class="todo-item">' + inputdata[index].text + '</div> <div class="buttons" id="1"> <button class="delete" type="button"></button> <input class="check" type="checkbox"></input> </div>';
-  newTodo.setAttribute('id', inputdata[index].id);
+  newTodo.innerHTML = '<div class="todo-item">' + element.text + '</div> <div class="buttons" id="1"> <button class="delete" type="button"></button> <input class="check" type="checkbox"></input> </div>';
+  newTodo.setAttribute('id', element.id);
   todolist.appendChild(newTodo);
   var checked = newTodo.querySelector('.check');
-  checked.checked = inputdata[index].completed;
+  checked.checked = element.completed;
+  inputField.value = '';
 }
 
+// function createDelete() {
+//
+// }
+
 function drawTasks(inputdata) {
-  inputdata.forEach(function (element, index) {
-    if ((inputdata[index].text).length > 0) {
+  inputdata.forEach(function (element) {
+    if ((element.text).length > 0) {
       console.log(element);
-      addHtml(inputdata, index);
+      addHtml(element);
     }
   });
 }
-// function requestHandler() {
-//   var xhr = new XMLHttpRequest();
-//   xhr.onload = function () {
-//     var inputdata = JSON.parse(xhr.response);
-//   };
-//   xhr.open('GET', 'https://mysterious-dusk-8248.herokuapp.com/todos/');
-//   xhr.send();
-// }
+
+function addTask() {
+  var xhr = new XMLHttpRequest();
+  xhr.open('POST', 'https://mysterious-dusk-8248.herokuapp.com/todos/');
+  xhr.setRequestHeader('Content-Type', 'application/json');
+  var inputFieldValue = inputField.value
+  console.log(inputFieldValue);
+  xhr.send(JSON.stringify({ "text": inputFieldValue }));
+  console.log(JSON.stringify({ "text": inputFieldValue }));
+  xhr.onload = function() {
+    if (xhr.readyState === xhr.DONE) {
+      addHtml(JSON.parse(xhr.response));
+      console.log(JSON.parse(xhr.response));
+    }
+  };
+}
 
 function getToDolList() {
   var xhr = new XMLHttpRequest();
-  xhr.onload = function () {
+  xhr.onload = function() {
     var inputdata = JSON.parse(xhr.response);
     drawTasks(inputdata);
   };
@@ -40,4 +53,6 @@ function getToDolList() {
   xhr.send();
 }
 
-button.addEventListener('click', getToDolList);
+getToDolList();
+
+button.addEventListener('click', addTask);
